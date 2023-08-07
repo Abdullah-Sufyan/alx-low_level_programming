db2f553241791457b5bad44bf186f48ba182f47c
Abdullah-Sufyan <hanesYL14@gmail.com>
2023-08-07 11:18:01 -0700

    task 0 - 4

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
+ * read_textfile- Reads text file display to STDOUT.
+ * @filename: The text file that is being read
+ * @letters: The number of letters to be read
+ * Return: w- the actual number of bytes read and displayed
  *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
